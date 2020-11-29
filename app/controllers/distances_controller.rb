0f6bc66dcf125bde171984a10ee88652e59cd342
time-shimizu <213.f.r48132@gmail.com>
2020-11-29 17:05:46 +0900

    削除できるよう修正

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -2,4 +2,10 @@ class DistancesController < ApplicationController
   def index
     @distances = Distance.all
   end
+
+  def destroy
+    Distance.find(params[:id]).destroy
+    flash[:success] = "削除しました"
+    redirect_to distances_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'distances/index'
   root 'sellinfos#new'
   resources :sellinfos
   resources :buyinfos
+  resources :distances
 end
